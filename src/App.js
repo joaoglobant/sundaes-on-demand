0c85de252901de5584fc703998db77ce0e81b892
Joao Paulo <joao.rodrigues@globant.com>
2021-09-10 09:49:15 -0300

    feature: setup routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
 import SummaryForm from './pages/SummaryForm/SummaryForm'
+import OrderSummary from './pages/OrderSummary/OrderSummary'
 
 function App() {
   return (
-    <div>
-      <SummaryForm />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/summary-form'>
+          <SummaryForm />
+        </Route>
+        <Route path='/order-summary'>
+          <OrderSummary />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
