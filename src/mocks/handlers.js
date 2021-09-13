ff282d10a5c825b766b567ed367975b2ce69fc1c
Joao Paulo <joao.rodrigues@globant.com>
2021-09-13 00:02:14 -0300

    test: create a mock toppings request

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,6 @@
 import { rest } from 'msw'
 
-const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'http://localhost:3030'
 
 export const handlers = [
   rest.get(`${BASE_URL}/scoops`, (req, res, ctx) => {
@@ -8,6 +8,16 @@ export const handlers = [
       ctx.json([
         { name: 'Chocolate', imagePath: '/images/chocolate.png' },
         { name: 'Vanilla', imagePath: '/images/vanilla.png' },
+      ])
+    )
+  }),
+
+  rest.get(`${BASE_URL}/toppings`, (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: 'Chocolate', imagePath: '/images/chocolate.png' },
+        { name: 'Vanilla', imagePath: '/images/vanilla.png' },
+        { name: 'Cherries', imagePath: '/images/cherries.png' },
       ])
     )
   }),
