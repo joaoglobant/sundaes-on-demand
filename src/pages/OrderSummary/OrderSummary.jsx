985b45d084ae80785d6bc773b21459738f6e5f46
Joao Paulo <joao.rodrigues@globant.com>
2021-09-13 00:03:13 -0300

    feature: create a test for OrderSummary component. Was tested the render of toppings and scoops items

--- src/pages/OrderSummary/OrderSummary.jsx
+++ src/pages/OrderSummary/OrderSummary.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Options from '../../components/Options/Options'
+import { BASE_URL } from '../../mocks/handlers'
+import api from '../../services/api'
 
 const OrderSummary = () => {
+  const [scoopsData, setScoopsData] = useState([])
+  const [toppingsData, setToppingsData] = useState([])
+  const [error, setError] = useState()
+
+  const getScoops = () => {
+    api
+      .get(`${BASE_URL}/scoops`)
+      .then(res => {
+        setScoopsData(res.data)
+      })
+      .catch(err => {
+        setError(err.response.data.message)
+      })
+  }
+
+  const getToppings = () => {
+    api
+      .get(`${BASE_URL}/toppings`)
+      .then(res => {
+        setToppingsData(res.data)
+      })
+      .catch(err => {
+        setError(err.response.data.message)
+      })
+  }
+
+  useEffect(() => {
+    getScoops()
+    getToppings()
+  }, [])
+
   return (
     <div style={{ padding: '50px' }}>
       <h3>Design your Sundae</h3>
@@ -9,25 +42,17 @@ const OrderSummary = () => {
         title='Scoops'
         eachValue='$2.00'
         totalValue='$6.00'
-        data={[
-          {
-            image: '',
-            label: 'Chocolate',
-          },
-        ]}
+        data={scoopsData}
       />
+      <p>{error}</p>
       <Options
         checkbox
         title='Toppings'
         eachValue='$1.50'
         totalValue='$4.50'
-        data={[
-          {
-            image: '',
-            label: 'Gummi bears',
-          },
-        ]}
+        data={toppingsData}
       />
+      <p>{error}</p>
     </div>
   )
 }
